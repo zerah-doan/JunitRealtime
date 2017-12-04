f89caebe492400e0b8f300eb375f69500c526513
Quang Doan <doanq@hsntech.com>
2017-12-04 16:07:45 +0700

    1st commit

---
+++ src/test/java/quang/doan/demo/DemoRealTimePlugin.java
@@ -0,0 +1,47 @@
+package quang.doan.demo;
+
+import org.testng.annotations.Test;
+
+public class DemoRealTimePlugin {
+
+	@Test
+	public void Test1() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void Test2() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void Test3() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void Test4() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
